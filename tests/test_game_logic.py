0d9dbd6a0e1cb96e9774167cf82fbd323cd4a1b1
DoodleTDT <tiot1833@gmail.com>
2026-08-27 15:18:20 -0500

    Added testing for resetting game

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -1,4 +1,6 @@
-from logic_utils import check_guess
+from app import st
+import random
+from logic_utils import check_guess, get_range_for_difficulty, update_score, parse_guess
 
 def test_winning_guess():
     # If the secret is 50 and guess is 50, it should be a win
@@ -14,3 +16,14 @@ def test_guess_too_low():
     # If secret is 50 and guess is 40, hint should be "Too Low"
     result = check_guess(40, 50)
     assert result == "Too Low"
+
+def test_new_game_after_wonORlost():
+    # After winning, starting a new game should reset the status to "playing"
+    st.session_state.status = "won"  # This will always be "won" due to Python's boolean evaluation
+    st.session_state.status = "lost"  # Overwrite to test the "lost" case
+    st.session_state.attempts = 0
+    st.session_state.secret = random.randint(1, 100)
+    st.session_state.score = 0
+    st.session_state.history = []
+    assert st.session_state.status == "playing"
+
